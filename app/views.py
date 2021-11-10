0850409d247ad8dc9e99b28ac7b3099e705f757f
kennedybabu <kennedynjogu8@gmail.com>
2021-11-10 15:11:16 +0300

    add app configurations

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+def index():
+    '''
+    View root function that returns the index page and its data
+    '''
+    title = 'Welcome, pomodoro'
+    return render_template('index.html', title = title)
+
