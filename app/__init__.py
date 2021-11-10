9339f3182852220b58e363cf7097e97d1faceaed
kennedybabu <kennedynjogu8@gmail.com>
2021-11-10 15:16:43 +0300

    create the homepage route

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from .config import DevConfig
 
 #initialising the app
 app = Flask(__name__)
+
+#setting up configurations
+app.config.from_object(DevConfig)
 
 from app import views
