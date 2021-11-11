01152649ed1b5bd9227feb2e4888066930872b10
kennedybabu <kennedynjogu8@gmail.com>
2021-11-11 11:14:17 +0300

    create the sessions database

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server', Server)
 
-migrate = Migrate(app)
+migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 
