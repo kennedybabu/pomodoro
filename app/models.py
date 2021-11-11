430803dc5523a0a8280c1dc2e06afc7652afd2c5
kennedybabu <kennedynjogu8@gmail.com>
2021-11-11 09:01:09 +0300

    create a session class

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from . import db
 
 
@@ -6,9 +7,22 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key =True)
     username = db.Column(db.String(255))
+    session = db.Relationship('Session', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Session(db.Model):
+    __tablename__ = 'sessions'
+
+    title = db.Column(db.String(255), unique=True)
+    work_time = db.Column(db.Integer)
+    break_time = db.Column(db.Integer)
+    break_activity = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f'Session {self.title}'
         
    
         
