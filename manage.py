fe058523598bb1f7cb3734783656a5d4e905933a
kennedybabu <kennedynjogu8@gmail.com>
2021-11-11 10:59:13 +0300

    init flask_migrate

--- manage.py
+++ manage.py
@@ -1,11 +1,15 @@
 from app import create_app, db
 from flask_script import Manager, Server
 from app.models import User, Session
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server', Server)
+
+migrate = Migrate(app)
+manager.add_command('db', MigrateCommand)
 
 
 @manager.shell
