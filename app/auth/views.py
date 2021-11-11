47a66f4afd8dc8741bc24b772c552c355d69df34
kennedybabu <kennedynjogu8@gmail.com>
2021-11-11 13:05:04 +0300

    implement user authentification

--- app/auth/views.py
+++ app/auth/views.py
@@ -34,7 +34,7 @@ def login():
         flash('Invalid username or Password')
 
     title = 'Pomodoro login'
-    return render_template('auth/login,html', login_form = login_form, title = title)
+    return render_template('auth/login.html', login_form = login_form, title = title)
 
 
 @auth.route('/logout')
