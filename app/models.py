38aef16607ab97989eeb5da2c74f8c3b0abeb7e1
kennedybabu <kennedynjogu8@gmail.com>
2021-11-11 11:28:47 +0300

    add user test for password security

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -8,6 +9,17 @@ class User(db.Model):
     username = db.Column(db.String(255))
     session = db.relationship('Session', backref='user', lazy='dynamic')
     pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
 
     def __repr__(self):
         return f'User {self.username}'
